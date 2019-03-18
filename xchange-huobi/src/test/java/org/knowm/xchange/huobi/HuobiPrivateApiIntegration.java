33c23707754edcdcdb04258dda1997687ccb3f15
snowflower2019 <lhbwyp_ems@163.com>
2019-03-18 18:39:41 +0800

    add test method.

--- xchange-huobi/src/test/java/org/knowm/xchange/huobi/HuobiPrivateApiIntegration.java
+++ xchange-huobi/src/test/java/org/knowm/xchange/huobi/HuobiPrivateApiIntegration.java
@@ -6,7 +6,12 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collection;
-import org.junit.*;
+
+import org.junit.After;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
 import org.knowm.xchange.currency.Currency;
@@ -24,101 +29,100 @@ import org.knowm.xchange.service.trade.TradeService;
 
 public class HuobiPrivateApiIntegration {
 
-  private HuobiProperties properties;
-  private Exchange exchange;
+	private HuobiProperties properties;
+	private Exchange exchange;
 
-  @Before
-  public void setup() throws IOException {
-    properties = new HuobiProperties();
-    Assume.assumeTrue("Ignore tests because credentials are missing", properties.isValid());
+	@Before
+	public void setup() throws IOException {
+		properties = new HuobiProperties();
+		Assume.assumeTrue("Ignore tests because credentials are missing", properties.isValid());
 
-    exchange =
-        ExchangeFactory.INSTANCE.createExchange(
-            HuobiExchange.class.getName(), properties.getApiKey(), properties.getSecretKey());
-  }
+		exchange = ExchangeFactory.INSTANCE.createExchange(HuobiExchange.class, properties.getApiKey(), properties.getSecretKey());
+	}
 
-  @After
-  public void teardown() throws IOException {
-    if (exchange != null) {
-      for (LimitOrder order : exchange.getTradeService().getOpenOrders().getOpenOrders()) {
-        exchange.getTradeService().cancelOrder(order.getId());
-      }
-    }
-  }
+	@After
+	public void teardown() throws IOException {
+		if (exchange != null) {
+			for (LimitOrder order : exchange.getTradeService().getOpenOrders().getOpenOrders()) {
+				exchange.getTradeService().cancelOrder(order.getId());
+			}
+		}
+	}
 
-  @Test
-  public void getAccountTest() throws IOException {
-    HuobiAccountService accountService = (HuobiAccountService) exchange.getAccountService();
-    HuobiAccount[] accounts = accountService.getAccounts();
-    System.out.println(Arrays.toString(accounts));
-  }
+	@Test
+	public void getAccountTest() {
+		try {
+			HuobiAccountService accountService = (HuobiAccountService) exchange.getAccountService();
+			HuobiAccount[] accounts = accountService.getAccounts();
+			System.out.println(Arrays.toString(accounts));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-  @Test
-  public void getBalanceTest() throws IOException {
-    AccountService accountService = exchange.getAccountService();
-    Balance balance = accountService.getAccountInfo().getWallet().getBalance(Currency.USDT);
-    System.out.println(balance.toString());
-    assertThat(balance).isNotNull();
-  }
+	@Test
+	public void getBalanceTest() throws IOException {
+		AccountService accountService = exchange.getAccountService();
+		Balance balance = accountService.getAccountInfo().getWallet().getBalance(Currency.ETH);
+		System.out.println(balance.toString());
+		assertThat(balance).isNotNull();
+	}
 
-  @Test
-  public void getOpenOrdersTest() throws IOException {
-    TradeService tradeService = exchange.getTradeService();
-    OpenOrders openOrders = tradeService.getOpenOrders();
-    System.out.println(openOrders.toString());
-    assertThat(openOrders).isNotNull();
-  }
+	@Test
+	public void getOpenOrdersTest() throws IOException {
+		TradeService tradeService = exchange.getTradeService();
+		OpenOrders openOrders = tradeService.getOpenOrders();
+		System.out.println(openOrders.toString());
+		assertThat(openOrders).isNotNull();
+	}
 
-  @Test
-  public void getOrderTest() throws IOException {
-    TradeService tradeService = exchange.getTradeService();
-    Collection<Order> orders = tradeService.getOrder("2132866355");
-    System.out.println(orders.toString());
-    assertThat(orders).isNotNull();
-  }
+	@Test
+	public void getOrderTest() throws IOException {
+		TradeService tradeService = exchange.getTradeService();
+		Collection<Order> orders = tradeService.getOrder("26981387170");
+		System.out.println(orders.toString());
+		assertThat(orders).isNotNull();
+	}
 
-  @Test
-  public void placeLimitOrderTest() throws IOException {
-    String orderId = placePendingOrder();
-    System.out.println(orderId);
-  }
+	@Test
+	public void placeLimitOrderTest() throws IOException {
+		String orderId = placePendingOrder();
+		System.out.println(orderId);
+	}
 
-  private String placePendingOrder() throws IOException {
-    TradeService tradeService = exchange.getTradeService();
-    HuobiAccountService accountService = (HuobiAccountService) exchange.getAccountService();
-    HuobiAccount[] accounts = accountService.getAccounts();
-    LimitOrder limitOrder =
-        new LimitOrder(
-            OrderType.BID,
-            new BigDecimal("0.001"),
-            new CurrencyPair("BTC", "USDT"),
-            String.valueOf(accounts[0].getId()),
-            null,
-            new BigDecimal("10000"));
-    return tradeService.placeLimitOrder(limitOrder);
-  }
+	private String placePendingOrder() throws IOException {
+		try {
+			TradeService tradeService = exchange.getTradeService();
+			HuobiAccountService accountService = (HuobiAccountService) exchange.getAccountService();
+			HuobiAccount[] accounts = accountService.getAccounts();
+			LimitOrder limitOrder = new LimitOrder(OrderType.BID, new BigDecimal("1.9"), new CurrencyPair("NAS", "ETH"), String.valueOf(accounts[0].getId()), null,
+					new BigDecimal("0.00500"));
+			return tradeService.placeLimitOrder(limitOrder);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-  @Test
-  public void placeMarketOrderTest() throws IOException {
-    TradeService tradeService = exchange.getTradeService();
-    HuobiAccountService accountService = (HuobiAccountService) exchange.getAccountService();
-    HuobiAccount[] accounts = accountService.getAccounts();
-    MarketOrder marketOrder =
-        new MarketOrder(
-            OrderType.ASK,
-            new BigDecimal("0.0002"),
-            new CurrencyPair("BTC", "USDT"),
-            String.valueOf(accounts[0].getId()),
-            null);
-    String orderId = tradeService.placeMarketOrder(marketOrder);
-    System.out.println(orderId);
-  }
+	@Test
+	public void placeMarketOrderTest() throws IOException {
+		TradeService tradeService = exchange.getTradeService();
+		HuobiAccountService accountService = (HuobiAccountService) exchange.getAccountService();
+		HuobiAccount[] accounts = accountService.getAccounts();
+		MarketOrder marketOrder = new MarketOrder(OrderType.ASK, new BigDecimal("0.0002"), new CurrencyPair("BTC", "USDT"), String.valueOf(accounts[0].getId()), null);
+		String orderId = tradeService.placeMarketOrder(marketOrder);
+		System.out.println(orderId);
+	}
 
-  @Test
-  @Ignore("Use it for manual")
-  public void cancelOrderTest() throws IOException {
-    TradeService tradeService = exchange.getTradeService();
-    boolean result = tradeService.cancelOrder("2134551697");
-    System.out.println(result);
-  }
+	@Test
+	@Ignore("Use it for manual")
+	public void cancelOrderTest() throws IOException {
+		try {
+			TradeService tradeService = exchange.getTradeService();
+			boolean result = tradeService.cancelOrder("26981387170");
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- xchange-huobi/src/test/java/org/knowm/xchange/huobi/HuobiProperties.java
+++ xchange-huobi/src/test/java/org/knowm/xchange/huobi/HuobiProperties.java
@@ -1,6 +1,5 @@
 package org.knowm.xchange.huobi;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,45 +7,42 @@ import java.util.Properties;
 
 class HuobiProperties {
 
-  private final String FILENAME = "huobi-secret.keys";
-  private final String API = "api-key";
-  private final String SECRET = "secret-key";
+	private final String FILENAME = "/huobi-secret.keys";
+	private final String API = "api-key";
+	private final String SECRET = "secret-key";
 
-  private String apiKey;
-  private String secretKey;
-  private boolean valid;
+	private String apiKey;
+	private String secretKey;
+	private boolean valid;
 
-  public HuobiProperties() throws IOException {
+	public HuobiProperties() throws IOException {
 
-    Properties properties = new Properties();
-    try (InputStream input = new FileInputStream(FILENAME)) {
-      properties.load(input);
-      apiKey = properties.getProperty(API);
-      secretKey = properties.getProperty(SECRET);
+		Properties properties = new Properties();
+		try (InputStream input = getClass().getResourceAsStream(FILENAME)) {
+			properties.load(input);
+			apiKey = properties.getProperty(API);
+			secretKey = properties.getProperty(SECRET);
 
-      if (apiKey == null || apiKey.isEmpty()) {
-        throw new IllegalArgumentException("API key is missing");
-      }
-      if (secretKey == null || secretKey.isEmpty()) {
-        throw new IllegalArgumentException("Secret key is missing");
-      }
-      valid = true;
-    } catch (FileNotFoundException ignored) {
-      valid = false;
-      apiKey = null;
-      secretKey = null;
-    }
-  }
+			if (apiKey == null || apiKey.isEmpty()) { throw new IllegalArgumentException("API key is missing"); }
+			if (secretKey == null || secretKey.isEmpty()) { throw new IllegalArgumentException("Secret key is missing"); }
+			valid = true;
+		} catch (FileNotFoundException ignored) {
+			ignored.printStackTrace();
+			valid = false;
+			apiKey = null;
+			secretKey = null;
+		}
+	}
 
-  public boolean isValid() {
-    return valid;
-  }
+	public boolean isValid() {
+		return valid;
+	}
 
-  public String getApiKey() {
-    return apiKey;
-  }
+	public String getApiKey() {
+		return apiKey;
+	}
 
-  public String getSecretKey() {
-    return secretKey;
-  }
+	public String getSecretKey() {
+		return secretKey;
+	}
 }
